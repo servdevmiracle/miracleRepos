03780d16853bf1297ca5ff04f36fc2fa5cb7cf09
shjo <servdevmiracle@gmail.com>
2019-03-08 13:59:09 +0900

    CRUD 샘플코드 완료

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -4,45 +4,77 @@ const mongoose = require('mongoose');
 const Employee = mongoose.model('Employee');
 
 router.get('/', (req, res) => {
-    res.render("employee/addOrEdit",{
-        viewTitle : "InsertEmployee"
-    })
+    res.render("employee/addOrEdit", {
+        viewTitle: "Insert Employee"
+    });
 });
 
 router.post('/', (req, res) => {
-    InsertRecord(req,res);
+    if (req.body._id == '')
+        insertRecord(req, res);
+        else
+        updateRecord(req, res);
 });
 
-function InsertRecord(req, res) {
+
+function insertRecord(req, res) {
     var employee = new Employee();
     employee.fullName = req.body.fullName;
     employee.email = req.body.email;
     employee.mobile = req.body.mobile;
     employee.city = req.body.city;
-    console.log(req.body);
     employee.save((err, doc) => {
-        if (!err) {
+        if (!err)
             res.redirect('employee/list');
-        } else {
+        else {
             if (err.name == 'ValidationError') {
-                handleValidationError(err, body);
-                res.render("employee/addOrEdit",{
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
                     viewTitle: "Insert Employee",
-                    employee: req.body 
+                    employee: req.body
                 });
             }
-            console.log('Error during record insertion : ' + err);
+            else
+                console.log('Error during record insertion : ' + err);
         }
     });
 }
 
+function updateRecord(req, res) {
+    Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('employee/list'); }
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: 'Update Employee',
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record update : ' + err);
+        }
+    });
+}
+
+
 router.get('/list', (req, res) => {
-    res.json('from list');
+    Employee.find((err, docs) => {
+        if (!err) {
+            res.render("employee/list", {
+                list: docs
+            });
+        }
+        else {
+            console.log('Error in retrieving employee list :' + err);
+        }
+    });
 });
+
 
 function handleValidationError(err, body) {
     for (field in err.errors) {
-        switch (err.errors) {
+        switch (err.errors[field].path) {
             case 'fullName':
                 body['fullNameError'] = err.errors[field].message;
                 break;
@@ -53,7 +85,26 @@ function handleValidationError(err, body) {
                 break;
         }
     }
-    err.errors
 }
+
+router.get('/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("employee/addOrEdit", {
+                viewTitle: "Update Employee",
+                employee: doc
+            });
+        }
+    });
+});
+
+router.get('/delete/:id', (req, res) => {
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/employee/list');
+        }
+        else { console.log('Error in employee delete :' + err); }
+    });
+});
 
 module.exports = router;
